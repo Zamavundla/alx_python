7da2e35c9963e6207fe2fd94e8fd7aa6cc8c5f7c
Zamavundla <zsdouzee8@gmail.com>
2023-07-22 01:25:39 +0200

    Task 3 reversing a string

---
+++ python-functions/3-string.py
@@ -0,0 +1,8 @@
+def reverse_string(string):
+    reversed_string = string[::-1]
+    return reversed_string
+
+
+input_string = "Hello, World!"
+reversed_string = reverse_string(input_string)
+print(reversed_string)  # Output: "!dlroW ,olleH"
