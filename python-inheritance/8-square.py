487ee4eadb40cbbb1979cd8299990a80ba5d0eab
Zamavundla <zsdouzee8@gmail.com>
2023-08-03 22:49:27 +0200

    Task 8

---
+++ python-inheritance/8-square.py
@@ -0,0 +1,45 @@
+class BaseGeometry:
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    def __init__(self, width, height):
+        self.__width = 0
+        self.__height = 0
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+
+
+class Square(Rectangle):
+    def __init__(self, size):
+        self.__size = 0
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def __str__(self):
+        return "[Square] {}/{}".format(self.__size, self.__size)
+
+
+s1 = Square(5)
+print(s1)  # Output: [Square] 5/5
+print(s1.area())  # Output: 25
+
+s2 = Square(10)
+print(s2)  # Output: [Square] 10/10
+print(s2.area())  # Output: 100
