892bfbb6fdcdb34f97777847d77ca96ff9cf7d02
Zamavundla <zsdouzee8@gmail.com>
2023-08-03 19:47:59 +0200

    Task 3

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -3,13 +3,14 @@ import sys
 # Get the number of arguments
 num_args = len(sys.argv) - 1
 
-# Print the number of arguments
+# Print the number of arguments and the corresponding text
 if num_args == 0:
-    print("Number of argument(s): 0.")
+    print("0 arguments.")
+elif num_args == 1:
+    print("1 argument:")
 else:
-    print(f"Number of argument(s): {num_args}.")
-    print("Arguments:")
+    print(f"{num_args} arguments:")
 
-    # Print the list of arguments with their positions
-    for i, arg in enumerate(sys.argv[1:], 1):
-        print(f"{i}: {arg}")
+# Print the list of arguments with their positions
+for i, arg in enumerate(sys.argv[1:], 1):
+    print(f"{i}: {arg}")
