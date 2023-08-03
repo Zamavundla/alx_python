3c39bec3345d4a5d5b2519e6fc3cecac642e14ff
Zamavundla <zsdouzee8@gmail.com>
2023-08-03 21:31:00 +0200

    .

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -5,3 +5,26 @@ class Square:
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
+mysquare = Square(3)
+print(type(mysquare))  # Output: <class '__main__.Square'>
+print(mysquare.__dict__)  # Output: {'_Square__size': 3}
+
+# Correct output - case: mysquare = Square(89)
+mysquare = Square(89)
+print(type(mysquare))  # Output: <class '__main__.Square'>
+print(mysquare.__dict__)  # Output: {'_Square__size': 89}
+
+# Try to access the 'size' attribute directly (Not recommended)
+# This will raise an AttributeError since 'size' is a private attribute and not directly accessible
+try:
+    print(mysquare.size)
+except AttributeError as e:
+    print(e)  # Output: 'Square' object has no attribute 'size'
+
+# Try to access the '_size' attribute directly (Not recommended)
+# This will raise an AttributeError since '_size' is a private attribute and not directly accessible
+try:
+    print(mysquare._size)
+except AttributeError as e:
+    print(e)  # Output: 'Square' object has no attribute '_size'
