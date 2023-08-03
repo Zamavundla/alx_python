207e3245f42e548e55b07e33f9d721a842c750b2
Zamavundla <zsdouzee8@gmail.com>
2023-08-03 22:34:44 +0200

    .

--- python-inheritance/4-base_geometry.py
+++ python-inheritance/4-base_geometry.py
@@ -3,3 +3,11 @@ class BaseGeometry:
     
     def area(self):
         raise Exception("area() is not implemented")
+
+bg = BaseGeometry()
+print(dir(bg))
+
+try:
+    print(bg.area())
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
