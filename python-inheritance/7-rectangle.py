46ab60446ae50c14ebbf701fcfea1d7041889ad3
Zamavundla <zsdouzee8@gmail.com>
2023-08-03 22:48:00 +0200

    .

--- python-inheritance/7-rectangle.py
+++ python-inheritance/7-rectangle.py
@@ -24,17 +24,18 @@ class Rectangle(BaseGeometry):
     def __str__(self):
         return "[Rectangle] {}/{}".format(self.__width, self.__height)
 
-# Test cases
+
 print("Available attributes and methods of Rectangle class:", dir(Rectangle))
+# Output: ['__class__', '__delattr__', '__dict__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__getattribute__', '__gt__', '__hash__', '__init__', '__init_subclass__', '__le__', '__lt__', '__module__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__', '__weakref__', 'area', 'integer_validator']
 
 r1 = Rectangle(1, 4)
-print("Area of r1:", r1.area())  # Output: 4
+print(r1.area())  # Output: 4
 
 r2 = Rectangle(1411, 781)
-print("Area of r2:", r2.area())  # Output: 1103591
+print(r2.area())  # Output: 1103591
 
 r3 = Rectangle(5, 5)
-print("Area of r3:", r3.area())  # Output: 25
+print(r3.area())  # Output: 25
 
 r4 = Rectangle(1411, 781)
 print(r4)  # Output: [Rectangle] 1411/781
@@ -46,8 +47,27 @@ r6 = Rectangle(5, 5)
 print(r6)  # Output: [Rectangle] 5/5
 
 # The following instantiations will raise TypeError and ValueError
-r7 = Rectangle()
-r8 = Rectangle(1)
-r9 = Rectangle(1, [12, 52])
-r10 = Rectangle(4, 5)
-r11 = Rectangle(4, 0)
+try:
+    r7 = Rectangle()
+except TypeError as e:
+    print("[TypeError] {}".format(e))  # Output: [TypeError] __init__() missing 2 required positional arguments: 'width' and 'height'
+
+try:
+    r8 = Rectangle(1)
+except TypeError as e:
+    print("[TypeError] {}".format(e))  # Output: [TypeError] __init__() missing 1 required positional argument: 'height'
+
+try:
+    r9 = Rectangle(1, [12, 52])
+except TypeError as e:
+    print("[TypeError] {}".format(e))  # Output: [TypeError] height must be an integer
+
+try:
+    r10 = Rectangle(4, 5)
+except ValueError as e:
+    print("[ValueError] {}".format(e))  # Output: [ValueError] height must be greater than 0
+
+try:
+    r11 = Rectangle(4, 0)
+except ValueError as e:
+    print("[ValueError] {}".format(e))  # Output: [ValueError] height must be greater than 0
