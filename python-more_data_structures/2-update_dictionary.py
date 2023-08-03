1396563a23410e05123b7dced832cf377268f798
Zamavundla <zsdouzee8@gmail.com>
2023-08-03 20:53:32 +0200

    Task 2

---
+++ python-more_data_structures/2-update_dictionary.py
@@ -0,0 +1,17 @@
+def update_dictionary(a_dictionary, key, value):
+    a_dictionary[key] = value
+
+#dictionary1 = {'name': 'John', 'age': 25}
+#update_dictionary(dictionary1, 'age', 30)
+#print(dictionary1)
+# Output: {'name': 'John', 'age': 30}
+
+#dictionary2 = {'name': 'Alice'}
+#update_dictionary(dictionary2, 'age', 20)
+#print(dictionary2)
+# Output: {'name': 'Alice', 'age': 20}
+
+#dictionary3 = {}
+#update_dictionary(dictionary3, 'subject', 'Math')
+#print(dictionary3)
+# Output: {'subject': 'Math'}
