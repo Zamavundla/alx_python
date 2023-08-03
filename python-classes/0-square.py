eb05d77e67478c07e359b60f3b850f12a9d77fbd
Zamavundla <zsdouzee8@gmail.com>
2023-08-03 21:28:38 +0200

    .

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -2,15 +2,6 @@ class Square:
     def __init__(self, size):
         self.__size = size
 
-# Correct output - case: mysquare = Square(3)
-mysquare = Square(3)
-print(type(mysquare))  # Output: <class '__main__.Square'>
-print(mysquare.__dict__)  # Output: {'_Square__size': 3}
-
-# Correct output - case: mysquare = Square(89)
-mysquare = Square(89)
-print(type(mysquare))  # Output: <class '__main__.Square'>
-print(mysquare.__dict__)  # Output: {'_Square__size': 89}
 
 # Try to access the 'size' attribute directly (Not recommended)
 # This will raise an AttributeError since 'size' is a private attribute and not directly accessible
