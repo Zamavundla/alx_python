c4ce2c47b3745bfd005faa698bb167db20187a9d
Zamavundla <zsdouzee8@gmail.com>
2023-08-03 22:38:52 +0200

    .

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -9,3 +9,37 @@ class BaseGeometry:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
+
+bg = BaseGeometry()
+print(dir(bg))
+
+bg = BaseGeometry()
+bg.integer_validator("myint", 12)
+
+bg = BaseGeometry()
+bg.integer_validator("myint", 12)
+bg.integer_validator("width", 89)
+
+bg = BaseGeometry()
+try:
+    bg.integer_validator("name", "John")
+except TypeError as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
+
+bg = BaseGeometry()
+try:
+    bg.integer_validator("age", 0)
+except ValueError as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
+
+bg = BaseGeometry()
+try:
+    bg.integer_validator("age", -4)
+except ValueError as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
+
+bg = BaseGeometry()
+try:
+    bg.integer_validator("age", 13.5)
+except TypeError as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
