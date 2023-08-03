117b686c5eb436e2602c8033091666b21442e912
Zamavundla <zsdouzee8@gmail.com>
2023-08-03 21:42:10 +0200

    .

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -24,25 +24,24 @@ class Square:
         for _ in range(self.__size):
             print("#" * self.__size)
 
+
 mysquare = Square(3)
 print(type(mysquare))  # Output: <class '__main__.Square'>
 print(mysquare.__dict__)  # Output: {'_Square__size': 3}
+mysquare.my_print()  # Output: ###
+                    #         ###
+                    #         ###
 
 # Correct output - case: mysquare = Square(89)
 mysquare = Square(89)
 print(type(mysquare))  # Output: <class '__main__.Square'>
 print(mysquare.__dict__)  # Output: {'_Square__size': 89}
-
-# Try to access the 'size' attribute directly (Not recommended)
-# This will raise an AttributeError since 'size' is a private attribute and not directly accessible
-try:
-    print(mysquare.size)
-except AttributeError as e:
-    print(e)  # Output: 'Square' object has no attribute 'size'
-
-# Try to access the '_size' attribute directly (Not recommended)
-# This will raise an AttributeError since '_size' is a private attribute and not directly accessible
-try:
-    print(mysquare._size)
-except AttributeError as e:
-    print(e)  # Output: 'Square' object has no attribute '_size'
+mysquare.my_print()  # Output: ##########
+                    #         ##########
+                    #         ##########
+                    #         ##########
+                    #         ##########
+                    #         ##########
+                    #         ##########
+                    #         ##########
+                    #         ##########
