98d72356262305d7fb88a41622262fb3b1e94d17
Zamavundla <zsdouzee8@gmail.com>
2023-08-13 14:35:57 +0200

    .

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -1,14 +1,14 @@
 import random
 
 number = random.randint(-10000, 10000)
-last_digit = abs(number) % 10  
+last_digit = abs(number) % 10  # Get the last digit
 
 message = "Last digit of {}\nis {}\nand is".format(number, last_digit)
 
-if last_digit > 5:
-    message += " greater than 5"
-elif last_digit == 0:
+if last_digit == 0:
     message += " 0"
+elif last_digit > 5:
+    message += " greater than 5"
 else:
     message += " less than 6 and not 0"
 
