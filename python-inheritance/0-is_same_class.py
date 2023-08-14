beefe32351ec976a6bde6a5522da0f68a46fcb10
Zamavundla <zsdouzee8@gmail.com>
2023-08-14 17:34:27 +0200

    .

--- python-inheritance/0-is_same_class.py
+++ python-inheritance/0-is_same_class.py
@@ -1,14 +1,20 @@
+"""This module will return True if the object is an instance of 
+a specified class either False"""
 def is_same_class(obj, a_class):
+    """A prototype for instance, which defines an object and a class"""
     
     if type(obj) is a_class:
+        """This defines if the object is exactly a class and return true"""
         return True
+    
     else:
+        """If the object is not a class therefore false must be returned"""
         return False
 
-a = 1
-if is_same_class(a, int):
-    print("{} is an instance of the class {}".format(a, int.__name__))
-if is_same_class(a, float):
-    print("{} is an instance of the class {}".format(a, float.__name__))
-if is_same_class(a, object):
-    print("{} is an instance of the class {}".format(a, object.__name__))
+obj = 1
+if is_same_class(obj, int):
+    print("{} is an instance of the class {}".format(obj, int.__name__))
+if is_same_class(obj, float):
+    print("{} is an instance of the class {}".format(obj, float.__name__))
+if is_same_class(obj, object):
+    print("{} is an instance of the class {}".format(obj, int.__name__))
