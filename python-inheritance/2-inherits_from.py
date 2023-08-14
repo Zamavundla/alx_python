be6625730acee1781447681c23e22c4ce716eaaa
Zamavundla <zsdouzee8@gmail.com>
2023-08-14 18:32:40 +0200

    .

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -1,43 +1,10 @@
 def inherits_from(obj, a_class):
-    """
-    Check if an object is an instance of a class that inherited (directly or indirectly)
-    from the specified class.
-
-    :param obj: The object to be checked.
-    :param a_class: The class to compare against.
-    :return: True if the object is an instance of a subclass of the specified class, otherwise False.
-    """
-    return issubclass(type(obj), a_class) and type(obj) is not a_class
-
-# Test cases
-a = 1
-if inherits_from(a, int):
-    print("{} inherited from class {}".format(a, int.__name__))
+    return isinstance(obj, a_class) and type(obj) is not a_class
 
 a = True
 if inherits_from(a, int):
     print("{} inherited from class {}".format(a, int.__name__))
-
-a = 3.14
-if inherits_from(a, int):
-    print("{} inherited from class {}".format(a, int.__name__))
-
-a = True
-if inherits_from(a, int):
-    print("{} inherited from class {}".format(a, int.__name__))
-
-a = None
-if inherits_from(a, int):
-    print("{} inherited from class {}".format(a, int.__name__))
-
-a = None
-if inherits_from(a, int):
-    print("{} inherited from class {}".format(a, int.__name__))
-
-a = [1, 2, 3]
-if inherits_from(a, int):
-    print("{} inherited from class {}".format(a, int.__name__))
-
-a = [1, 2, 3]
-if inherits_from(a, int):
-    print("{} inherited from class {}".format(a, int.__name__))
+if inherits_from(a, bool):
+    print("{} inherited from class {}".format(a, bool.__name__))
+if inherits_from(a, object):
+    print("{} inherited from class {}".format(a, object.__name__))
