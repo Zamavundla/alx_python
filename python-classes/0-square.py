ad436ae5f57e995b81d0845f375c132605268fcd
Zamavundla <zsdouzee8@gmail.com>
2023-08-14 19:13:09 +0200

    .

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -14,3 +14,37 @@ class Square:
             size (int): The size of the square.
         """
         self.__size = size
+
+    def dict_(self):
+        """
+        Returns a dictionary representation of the square instance.
+
+        Returns:
+            dict: A dictionary containing the size of the square.
+        """
+        return {"size": self.__size}
+
+if __name__ == "__main__":
+    mysquare = Square(3)
+    print(type(mysquare))
+    print(mysquare.dict_())
+
+    mysquare = Square(89)
+    print(type(mysquare))
+    print(mysquare.dict_())
+
+    mysquare = Square(3)
+    print(type(mysquare))
+    print(mysquare.dict_())
+
+    try:
+        print(mysquare.size)
+    except Exception as e:
+        print(e)
+
+    mysquare = Square(3)
+    print(type(mysquare))
+    try:
+        print(mysquare._size)
+    except Exception as e:
+        print(e)
