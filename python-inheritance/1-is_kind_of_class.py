560777097ffa769123d36314e37e81b7ba18e384
Zamavundla <zsdouzee8@gmail.com>
2023-08-14 18:01:08 +0200

    .

--- python-inheritance/1-is_kind_of_class.py
+++ python-inheritance/1-is_kind_of_class.py
@@ -1,6 +1,3 @@
-# This module defines a function that checks if an object is an instance of a specified
-# class or its subclasses.
-
 def is_kind_of_class(obj, a_class):
     """
     Check if an object is an instance of, or if the object is an instance of a class
@@ -8,40 +5,32 @@ def is_kind_of_class(obj, a_class):
 
     :param obj: The object to be checked.
     :param a_class: The class to compare against.
-    :return: True if the object is an instance of the specified class or its subclass, 
+    :return: True if the object is an instance of the specified class or its subclass,
     otherwise False.
     """
     return isinstance(obj, a_class)
 
 # Test cases
 a = 1
-if is_kind_of_class(a, int):
-    print("{} comes from {}".format(a, int.__name__))
+print(is_kind_of_class(a, int))  # True
 
 a = True
-if is_kind_of_class(a, int):
-    print("{} comes from {}".format(a, int.__name__))
+print(is_kind_of_class(a, int))  # False
 
 a = 3.14
-if is_kind_of_class(a, int):
-    print("{} comes from {}".format(a, int.__name__))
+print(is_kind_of_class(a, int))  # False
 
 a = True
-if is_kind_of_class(a, object):
-    print("{} comes from {}".format(a, object.__name__))
+print(is_kind_of_class(a, object))  # True
 
 a = None
-if is_kind_of_class(a, object):
-    print("{} comes from {}".format(a, object.__name__))
+print(is_kind_of_class(a, object))  # True
 
 a = None
-if is_kind_of_class(a, list):
-    print("{} comes from {}".format(a, list.__name__))
+print(is_kind_of_class(a, list))  # False
 
 a = [1, 2, 3]
-if is_kind_of_class(a, list):
-    print("{} comes from {}".format(a, list.__name__))
+print(is_kind_of_class(a, list))  # True
 
 a = [1, 2, 3]
-if is_kind_of_class(a, object):
-    print("{} comes from {}".format(a, object.__name__))
+print(is_kind_of_class(a, object))  # True
