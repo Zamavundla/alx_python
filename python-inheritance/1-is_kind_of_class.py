05f2ce4d6ebeaf88aba13cad0c395943dd01675c
Zamavundla <zsdouzee8@gmail.com>
2023-08-14 18:14:17 +0200

    updates

--- python-inheritance/1-is_kind_of_class.py
+++ python-inheritance/1-is_kind_of_class.py
@@ -5,9 +5,7 @@ def is_kind_of_class(obj, a_class):
 
     :param obj: The object to be checked.
     :param a_class: The class to compare against.
-    :return: 
-    True if the object is an instance of the specified class or its subclass, otherwise
-    False.
+    :return: True if the object is an instance of the specified class or its subclass, otherwise False.
     """
     return isinstance(obj, a_class)
 
@@ -15,13 +13,47 @@ def is_kind_of_class(obj, a_class):
 a = 1
 if is_kind_of_class(a, int):
     print(True)
+else:
+    print(False)
 
 a = True
 if is_kind_of_class(a, int):
     print(True)
+else:
+    print(False)
 
 a = 3.14
 if is_kind_of_class(a, int):
+    print(True)
+else:
+    print(False)
+
+a = True
+if is_kind_of_class(a, object):
+    print(True)
+else:
+    print(False)
+
+a = None
+if is_kind_of_class(a, object):
+    print(True)
+else:
+    print(False)
+
+a = None
+if is_kind_of_class(a, list):
+    print(False)
+else:
+    print(True)
+
+a = [1, 2, 3]
+if is_kind_of_class(a, list):
+    print(True)
+else:
+    print(False)
+
+a = [1, 2, 3]
+if is_kind_of_class(a, object):
     print(True)
 else:
     print(False)
