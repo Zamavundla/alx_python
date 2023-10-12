65daa999b09862a8e248126f4c18bc310359e7ea
Zamavundla <zsdouzee8@gmail.com>
2023-10-12 22:31:28 +0200

    .

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -23,7 +23,7 @@ def get_employee_todo_progress(employee_id):
     print(f"Employee {employee_name} is done with tasks({completed_tasks}/{total_tasks}):")
     for task in todo_data:
         if task['completed']:
-            print(f"\t{task['title']}")
+            print(f"\t {task['title']}")
 
 if __name__ == "__main__":
     import sys
