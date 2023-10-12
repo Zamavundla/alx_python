66b9969d80695e14396de3f7c78b00046bd3470c
Zamavundla <zsdouzee8@gmail.com>
2023-10-12 22:26:31 +0200

    .

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -19,12 +19,22 @@ def get_employee_todo_progress(employee_id):
     total_tasks = len(todo_data)
     completed_tasks = sum(1 for task in todo_data if task['completed'])
 
-    # Print the progress
-    print(f"Employee {employee_name} is done with tasks ({completed_tasks}/{total_tasks}):")  # noqa: E501
+    # Print the progress in the requested format
+    print(f"Employee {employee_name} is done with tasks({completed_tasks}/{total_tasks}):")
     for task in todo_data:
         if task['completed']:
             print(f"\t{task['title']}")
 
 if __name__ == "__main__":
-    employee_id = int(input("Enter the employee ID: "))
+    import sys
+    if len(sys.argv) != 2:
+        print("Usage: python3 gather_data_from_an_API.py <employee_id>")
+        sys.exit(1)
+
+    try:
+        employee_id = int(sys.argv[1])
+    except ValueError:
+        print("Employee ID must be an integer.")
+        sys.exit(1)
+
     get_employee_todo_progress(employee_id)
